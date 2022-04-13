b5fa9bb59d62541c025089461bed9257bcf8b0bd
Dean Pierce <pierce403@gmail.com>
2022-04-13 01:19:02 -0700

    silly tool for converting IPs to domains

---
+++ ip2domain.py
@@ -0,0 +1,20 @@
+import socket, ssl
+import OpenSSL # pip install pyopenssl
+import sys
+
+# https://stackoverflow.com/questions/7689941/how-can-i-retrieve-the-tls-ssl-peer-certificate-of-a-remote-host-using-python
+# https://www.pyopenssl.org/en/stable/api/crypto.html
+
+if len(sys.argv) != 2:
+  print("usage: "+sys.argv[0]+" <list_of_ips.txt> "+str(len(sys.argv)) )
+  sys.exit()
+
+ips = open(sys.argv[1], 'r')
+
+for ip in ips:
+  cert = ssl.get_server_certificate((ip.strip(), 443))
+  x509 = OpenSSL.crypto.load_certificate(OpenSSL.crypto.FILETYPE_PEM, cert)
+  comps = x509.get_subject().get_components()
+  for comp in comps:
+    if comp[0].decode() == "CN":
+      print(comp[1].decode())
